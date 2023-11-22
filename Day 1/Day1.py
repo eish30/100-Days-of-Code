a98ecc762b6db9b1dd766c7f4991dd862ccd8587
Eishani Bhattacharya <113579065+eish30@users.noreply.github.com>
2023-11-23 00:54:01 +0530

    Update Day1.py

--- Day 1/Day1.py
+++ Day 1/Day1.py
@@ -1,6 +1,12 @@
+#band name generator
+
 print("Welcome to Band Name Generator!")
+
+#takes inputs
 print("What's the name of the city you grew up in?")
 city = input()
 print("What's your pet's name?")
 petname = input()
+
+#generates name
 print("Your band name could be", city, petname)
